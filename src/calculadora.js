11cc6f65ef912b0c64bc7926444a551ebd773303
ALEXSON LUNA <alexsonluna@gail.com>
2024-09-14 15:12:36 +0000

    Funcao Potenciacao

--- src/calculadora.js
+++ src/calculadora.js
@@ -31,3 +31,15 @@
       multiply
     };
   
+   // Função de potenciação
+    function power(base, exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    // Exportar as funções
+    module.exports = {
+      add,
+      subtract,
+      multiply,
+      power
+    };
